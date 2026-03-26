0dcd4f3f73468303f15e613bb6c6440ef7ee5db9
DNG222-code <dnagy20474@iesjoanramis.org>
2026-03-26 19:43:46 +0100

    Activity_9.sql

--- BD/UD6/Activity_9.sql
+++ BD/UD6/Activity_9.sql
@@ -13,7 +13,44 @@ WHERE EXISTS (
 
 -- INNER JOIN
 SELECT * FROM Agency a
-INNER JOIN Reservation r ON a.id_agency = r.id_agency
-WHERE a.id_agency IS NOT NULL
+INNER JOIN Reservation r ON a.id_agency = r.id_agency;
 
 -- 2
+
+-- SUBQUERY
+SELECT * FROM Customer c
+WHERE EXISTS (
+    SELECT c.id_customer
+    FROM Reservation r
+    WHERE r.id_customer = c.id_customer
+      AND r.car_delivered = 'delivered'
+);
+
+-- INNER JOIN
+SELECT * FROM Customer c
+INNER JOIN Reservation r ON c.id_customer = r.id_customer
+WHERE r.car_delivered = 'delivered';
+
+-- 3
+
+-- SUBQUERY
+SELECT * FROM Customer c
+WHERE NOT EXISTS(
+    SELECT c.id_customer
+    FROM Reservation r
+    WHERE c.id_customer = r.id_customer
+);
+
+-- INNER JOIN
+SELECT * FROM Customer c
+LEFT JOIN Reservation r ON c.id_customer = r.id_customer
+WHERE r.id_reservation IS NULL;
+
+-- 4
+
+-- SUBQUERY
+SELECT * FROM Customer c
+WHERE EXISTS(
+    SELECT c.id_customer
+    FROM
+)
