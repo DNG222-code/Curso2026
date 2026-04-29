cf06cecb3b0dcbd6d4d575746b70a6678e20a4ff
DNG222-code <dnagy20474@iesjoanramis.org>
2026-04-29 20:34:28 +0200

    Activity_7.sql.

---
+++ BD/UD7/Activity_7.sql
@@ -0,0 +1,78 @@
+-- Exercise 1 – IFNULL (total duration)
+
+DELIMITER //
+
+CREATE PROCEDURE total_duration_director (
+    IN p_id_director INT,
+    OUT p_total_duration INT
+)
+BEGIN
+    DECLARE v_total_duration INT;
+
+    SELECT IFNULL(SUM(duration_minutes), 0) AS 'total_duration'
+    INTO v_total_duration
+    FROM movie
+    WHERE director_id = p_id_director;
+
+    SET p_total_duration = v_total_duration;
+END //
+
+DELIMITER ;
+
+CALL total_duration_director(2, @total_duration);
+SELECT @total_duration;
+
+-- Exercise 2 – COALESCE (maximum salary)
+
+DELIMITER //
+
+CREATE PROCEDURE max_salary_movie (
+    IN p_id_movie INT,
+    OUT p_salary DECIMAL(10,2)
+)
+BEGIN
+    DECLARE v_max_salary DECIMAL(10,2);
+
+    SELECT COALESCE(MAX(salary), 0) AS 'max_salary'
+    INTO v_max_salary
+    FROM casting c
+    INNER JOIN movie m ON c.movie_id = m.movie_id
+    WHERE m.movie_id = p_id_movie;
+
+    SET p_salary = v_max_salary;
+END //
+
+DELIMITER ;
+
+CALL max_salary_movie(2, @salary);
+SELECT @salary;
+
+-- Exercise 3 – COALESCE + INOUT
+
+DELIMITER //
+
+CREATE PROCEDURE accumulate_salary_movie (
+    IN p_id_movie INT,
+    INOUT p_total DECIMAL(10,2)
+)
+BEGIN
+    DECLARE v_max_salary DECIMAL(10,2);
+    DECLARE v_avg_salary DECIMAL(10,2);
+
+    SELECT MAX(salary) AS 'max_salary'
+    INTO v_max_salary
+    FROM casting c
+    INNER JOIN movie m ON c.movie_id = m.movie_id
+    WHERE m.movie_id = p_id_movie;
+
+    SELECT AVG(salary) AS 'avg_salary'
+    INTO v_avg_salary
+    FROM casting c
+    INNER JOIN movie m ON c.movie_id = m.movie_id
+    WHERE m.movie_id = p_id_movie;
+
+    SET p_total = COALESCE(v_max_salary, v_avg_salary, 100, 0);
+
+END //
+
+DELIMITER ;
